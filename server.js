0839eb1f569b7526b401effd3f1da1313a841be7
Desert Python <mattsenneff@aol.com>
2020-09-22 21:20:32 -0700

    end of part 3

--- server.js
+++ server.js
@@ -20,3 +20,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+//mongo.connect?
