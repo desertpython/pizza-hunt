fc864f53e7c83d923a447c126b6bea40fe3dba13
Desert Python <mattsenneff@aol.com>
2020-09-22 17:26:18 -0700

    picking up more files

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,49 @@
 const { Schema, model } = require('mongoose');
-const PizzaSchema = new Schema({
-    pizzaName: {
+const Comment = require('./Comment');
+const moment = require('moment');
+
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
         type: String
-    },
-    createdBy: {
+      },
+      createdBy: {
         type: String
-    },
-    createdAt: {
+      },
+      createdAt: {
         type: Date,
-        default: Date.now
-    },
-    size: {
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      },
+      size: {
         type: String,
         default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
     },
-    toppings: []
-})
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false
+    }
+  );
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
+module.exports = { Pizza, Comment };
